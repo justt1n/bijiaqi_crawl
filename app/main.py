e4dc97b4e489bb6fb42bcfd197b3a3bfe42d5480
justt1n <tinnt0709@gmail.com>
2024-09-01 14:10:16 +0700

    edit skeleton

--- app/main.py
+++ app/main.py
@@ -1,32 +1,34 @@
-import platform
 import os
+import platform
 
 
 def check_system():
     system = platform.system().lower()
+    supported_systems = ["linux", "windows", "darwin"]
+    if system not in supported_systems:
+        print("System not supported")
+        exit(1)
     return system
+
 
 def get_drive():
     system = check_system()
-    if(system == "linux"):
+    if (system == "linux"):
         print("Open drive for linux...")
-        #TODO: Open drive for linux
-    elif(system == "windows"):
+        # TODO: Open drive for linux
+    elif (system == "windows"):
         print("Open drive for windows...")
-        #TODO: Open drive for windows
-    elif(system == "darwin"):
+        # TODO: Open drive for windows
+    elif (system == "darwin"):
         print("Open drive for mac...")
-        #TODO: Open drive for mac
-    else:
-        print("Unsupported system\nYour system is: {} ".format(system))
-    #TODO return drive
+        # TODO: Open drive for mac
+    # TODO return drive
     return None
 
 
 def get_gspread():
-    #TODO: Open gspread
+    # TODO: Open gspread
     pass
-
 
 
 def clear_screen():
@@ -38,37 +40,42 @@ def clear_screen():
 
 
 def read_data_from_sheet(gspread):
-    #TODO: Read data from sheet
+    # TODO: Read data from sheet
     pass
 
 
 def extract_data(data):
-    #TODO: Extract data from sheet to array of payload
+    # TODO: Extract data from sheet to array of payload
     pass
-    
+
+
 def write_data_to_sheet(gspread, data, line_number):
-    #TODO: Write data to sheet
+    # TODO: Write data to sheet
     pass
 
-def do_payload(driver, gspread):
-    the_url = "https://"
-    driver.get(the_url)
-    while True:
-        clear_screen()    
-        #TODO: Read data from sheet and turn into payload
-        data = read_data_from_sheet(gspread)
-        payload = extract_data(data)
-        #TODO: Use selenium to use that data
-        for payload_data in payload:
-            #TODO: Do something with payload then write to sheet
-            #Do something with payload
-            write_data_to_sheet(gspread, payload_data, 1)
-        
 
+def process(driver, gspread):
+    # TODO: Read data from sheet and turn into payload
+    data = read_data_from_sheet(gspread)
+    payload = extract_data(data)
+    # TODO: Use selenium to use that data
+    for payload_data in payload:
+        # TODO: Do something with payload then write to sheet
+        # Do something with payload
+        do_payload()
+        # Write to sheet
+        write_data_to_sheet(gspread, payload_data, 1)
+
+
+def do_payload():
+    pass
 
 
 def main():
     driver = get_drive()
     gspread = get_gspread()
-    do_payload(driver, gspread)
-        
+    the_url = "https://"
+    driver.get(the_url)
+    while (True):
+        clear_screen()
+        process(driver, gspread)
