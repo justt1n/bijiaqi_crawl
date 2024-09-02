40d589e4357477a8aafce7b119f04fbfe6b39542
justt1n <tinnt0709@gmail.com>
2024-09-02 10:31:25 +0700

    auto get drive

---
+++ app/GetDrive.py
@@ -0,0 +1,50 @@
+import wget
+import os
+import zipfile
+import platform
+
+def check_system():
+    system = platform.system().lower()
+    supported_systems = ["linux", "windows", "darwin"]
+    if system not in supported_systems:
+        print("System not supported")
+        exit(1)
+    return system
+
+def getDrive(os_type='windows'):
+    windowsUrl = "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_win32.zip"
+    
+    link = {
+        'windows': {
+            "url": "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_win32.zip",
+            "path": "../storage/windows.zip"
+        },
+        'linux': {
+            "url": "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip",
+            "path": "../storage/linux.zip"
+        },
+        'darwin': {
+            "url": "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_mac64.zip",
+            "path": "../storage/mac.zip"
+        }
+    }
+    
+    url = link[os_type]['url']
+    zip_path = link[os_type]['path']
+    
+    # Ensure the storage directory exists
+    os.makedirs(os.path.dirname(zip_path), exist_ok=True)
+    
+    wget.download(url, zip_path)
+    
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall('../storage/')
+    
+    os.remove(zip_path)
+    
+    print('Model downloaded and unzipped')
+    return 'Model downloaded and unzipped'
+
+if __name__ == "__main__":
+    os_type = check_system()
+    getDrive(os_type)
