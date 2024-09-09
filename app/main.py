14fe0698ae9cd0ac345d940921d86e0378a9a8c0
tinnt <tinnt@airtrip-intl.com>
2024-09-09 15:31:10 +0700

    add multi sheet process

--- app/main.py
+++ app/main.py
@@ -1,30 +1,33 @@
+import codecs
+import json
+import logging
 import os
 import platform
-import wget
+import time
 import zipfile
+from datetime import datetime
+
 import gspread
-import json
-import codecs
-from oauth2client.service_account import ServiceAccountCredentials
+import wget
 from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 import Payload
-import time
-import logging
-from datetime import datetime
 
 load_dotenv('settings.env')
 gsp = None
 driver = None
 HOST_DATA = None
+
 
 def setup_logging():
     # Load environment variables at the beginning of the script
@@ -41,12 +44,14 @@ def setup_logging():
 
     logging.basicConfig(filename=log_file, level=log_level, format=log_format)
 
+
 setup_logging()
+
 
 def print_function_name(func):
     def wrapper(*args, **kwargs):
         try:
-            log_message = f"Calling function: {func.__name__}"
+            log_message = f"Calling function: {func.__name__} with args: {args} and kwargs: {kwargs}"
             logging.info(log_message)
             return func(*args, **kwargs)
         except Exception as e:
@@ -54,6 +59,94 @@ def print_function_name(func):
             raise
 
     return wrapper
+
+
+## Retry functions
+
+def get_cell_text(cell, retries=3):
+    for _ in range(retries):
+        try:
+            return cell.text
+        except StaleElementReferenceException:
+            time.sleep(0.5)
+    raise StaleElementReferenceException("Failed to get cell text after retries")
+
+
+def get_row_elements(row, retries=3):
+    while retries > 0:
+        try:
+            return row.find_elements(By.TAG_NAME, 'td')
+        except StaleElementReferenceException:
+            retries -= 1
+            if retries == 0:
+                raise
+            time.sleep(0.5)
+
+
+def find_link_element(cell, retries=3):
+    for _ in range(retries):
+        try:
+            return cell.find_elements(By.TAG_NAME, 'a')[1]
+        except StaleElementReferenceException:
+            time.sleep(0.5)
+    raise StaleElementReferenceException("Failed to find link element after retries")
+
+
+def get_link_attribute(link_element, attribute='href', retries=3):
+    for _ in range(retries):
+        try:
+            return link_element.get_attribute(attribute)
+        except StaleElementReferenceException:
+            time.sleep(0.5)
+    raise StaleElementReferenceException(f"Failed to get attribute '{attribute}' after retries")
+
+
+def get_row_elements_with_retries(row, retries=3):
+    for _ in range(retries):
+        try:
+            return row.find_elements(By.TAG_NAME, 'td')
+        except StaleElementReferenceException:
+            time.sleep(0.5)
+    raise StaleElementReferenceException("Failed to find row elements after retries")
+
+
+def find_elements_with_retries(parent_element, by, value, retries=3):
+    for _ in range(retries):
+        try:
+            return parent_element.find_elements(by, value)
+        except StaleElementReferenceException:
+            time.sleep(0.5)
+    raise StaleElementReferenceException(f"Failed to find elements by {by}='{value}' after retries")
+
+
+@print_function_name
+def open_driver_with_retries(retries=3):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_service = Service(executable_path=driver_path)
+
+    for _ in range(retries):
+        try:
+            driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+            driver.get(os.getenv('DEFAULT_URL'))
+            return driver
+        except WebDriverException:
+            time.sleep(0.5)
+    raise WebDriverException("Failed to open driver and navigate to URL after retries")
+
+
+def do_payload_with_retries(payload, retries=3):
+    for _ in range(retries):
+        try:
+            return do_payload(payload)
+        except (StaleElementReferenceException, WebDriverException) as e:
+            logging.error(f"Error in do_payload: {e}")
+            time.sleep(1)
+    raise Exception("Failed to execute do_payload after retries")
+
+
+## End of retry functions
 
 
 @print_function_name
@@ -157,13 +250,13 @@ def clear_screen():
 
 
 @print_function_name
-def read_data_from_sheet():
+def read_data_from_sheet(sheet_name):
     def append_index_to_sheet_data(sheet_data):
         for index, row in enumerate(sheet_data):
             row.append(index + 1 + 1)
         return sheet_data
 
-    spread_sheet = gsp.open_by_url(os.getenv('SPREAD_SHEET_URL')).get_worksheet(0)
+    spread_sheet = gsp.open_by_url(os.getenv('SPREAD_SHEET_URL')).worksheet(sheet_name)
     data = spread_sheet.get_all_values()
     data = data[1:]
     data = append_index_to_sheet_data(data)
@@ -181,13 +274,13 @@ def extract_data(data):
 
 
 @print_function_name
-def write_data_to_sheet(data, line_number):
+def write_data_to_sheet(data, line_number, sheet_name):
     try:
         # Ensure line_number is an integer
         line_number = int(line_number)
         range_name = os.getenv('DESTINATION_RANGE').format(n=line_number)
         spread_sheet_url = os.getenv('SPREAD_SHEET_URL')
-        worksheet = gsp.open_by_url(spread_sheet_url).get_worksheet(0)
+        worksheet = gsp.open_by_url(spread_sheet_url).worksheet(sheet_name)
 
         # Convert data to a list of lists
         data_list = [data.toArray()]
@@ -202,80 +295,33 @@ def write_data_to_sheet(data, line_number):
 
 
 @print_function_name
-def process():
-    sheet_data = read_data_from_sheet()
+def process(sheet_name):
+    sheet_data = read_data_from_sheet(sheet_name)
     payloads = extract_data(sheet_data)
     for payload_data in payloads:
         ans = do_payload_with_retries(payload_data, retries=int(os.getenv('RETRIES_TIME')))
         if ans is not None:
             print(f"Found a match for {payload_data.name}")
-            write_data_to_sheet(ans, payload_data.sheet_row)
+            write_data_to_sheet(ans, payload_data.sheet_row, sheet_name)
             print(f"Match written to sheet at row {payload_data.sheet_row}")
 
-
-def get_cell_text(cell, retries=3):
-    for _ in range(retries):
+@print_function_name
+def multi_process():
+    sheets = os.getenv('SHEET_NAMES').split(',')
+    for sheet in sheets:
         try:
-            return cell.text
-        except StaleElementReferenceException:
-            time.sleep(0.5)
-    raise StaleElementReferenceException("Failed to get cell text after retries")
-
-
-def get_row_elements(row, retries=3):
-    while retries > 0:
-        try:
-            return row.find_elements(By.TAG_NAME, 'td')
-        except StaleElementReferenceException:
-            retries -= 1
-            if retries == 0:
-                raise
-            time.sleep(0.5)
-
-
-def find_link_element(cell, retries=3):
-    for _ in range(retries):
-        try:
-            return cell.find_elements(By.TAG_NAME, 'a')[1]
-        except StaleElementReferenceException:
-            time.sleep(0.5)
-    raise StaleElementReferenceException("Failed to find link element after retries")
-
-
-def get_link_attribute(link_element, attribute='href', retries=3):
-    for _ in range(retries):
-        try:
-            return link_element.get_attribute(attribute)
-        except StaleElementReferenceException:
-            time.sleep(0.5)
-    raise StaleElementReferenceException(f"Failed to get attribute '{attribute}' after retries")
-
-
-def get_row_elements_with_retries(row, retries=3):
-    for _ in range(retries):
-        try:
-            return row.find_elements(By.TAG_NAME, 'td')
-        except StaleElementReferenceException:
-            time.sleep(0.5)
-    raise StaleElementReferenceException("Failed to find row elements after retries")
-
-
-def find_elements_with_retries(parent_element, by, value, retries=3):
-    for _ in range(retries):
-        try:
-            return parent_element.find_elements(by, value)
-        except StaleElementReferenceException:
-            time.sleep(0.5)
-    raise StaleElementReferenceException(f"Failed to find elements by {by}='{value}' after retries")
-
+            process(sheet)
+        except Exception as e:
+            logging.error(f"Error in process: {e}")
+            driver.refresh()
 
 @print_function_name
 def do_payload(payload: Payload.Payload):
     retries_time = int(os.getenv('RETRIES_TIME'))
-    if int(payload.check) == 0:
+    if payload.check is None or int(payload.check) != 1:
         return
     host_id = payload.id
-    wait = WebDriverWait(driver, os.getenv('TIMEOUT'))
+    wait = WebDriverWait(driver, float(os.getenv('TIMEOUT')))
 
     host_id = str(host_id) + " "
     input_field = wait.until(EC.element_to_be_clickable((By.ID, 'speedhostname')))
@@ -339,44 +385,15 @@ def get_hostname_by_hostid(data, hostid):
     return None
 
 
-@print_function_name
-def open_driver_with_retries(retries=3):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_service = Service(executable_path=driver_path)
-
-    for _ in range(retries):
-        try:
-            driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-            driver.get(os.getenv('DEFAULT_URL'))
-            return driver
-        except WebDriverException:
-            time.sleep(0.5)
-    raise WebDriverException("Failed to open driver and navigate to URL after retries")
-
-
-def do_payload_with_retries(payload, retries=3):
-    for _ in range(retries):
-        try:
-            return do_payload(payload)
-        except (StaleElementReferenceException, WebDriverException) as e:
-            logging.error(f"Error in do_payload: {e}")
-            time.sleep(1)
-    raise Exception("Failed to execute do_payload after retries")
-
-
 if __name__ == "__main__":
     # load_dotenv('settings.env')
     driver_path = get_drive()
     gsp = get_gspread(os.getenv('KEY_PATH'))
     HOST_DATA = read_file_with_encoding(os.getenv('DATA_PATH'), encoding='utf-8')
 
-    sheet_data = read_data_from_sheet()
-    payloads = extract_data(sheet_data)
     driver = open_driver_with_retries(retries=int(os.getenv('RETRIES_TIME')))
 
     while (True):
         clear_screen()
-        process()
+        multi_process()
         time.sleep(int(os.getenv('REFRESH_TIME')))
